211a4d893523f6c46103a1abcc7aa34bd2bc95cd
narula98 <sahilnarula98@gmaill.com>
2022-07-02 12:52:18 +0530

    initialized repo

---
+++ src/test/java/com/app/onlinedelivery/onlinedelivery/OnlinedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.onlinedelivery.onlinedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
